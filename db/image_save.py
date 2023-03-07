0092a54a35617e535cb6e1ced98061bd277e0c30
victoria husemeyer <victoria_husemeyer@yahoo.com>
2023-03-07 10:22:27 +0000

    new code

--- db/image_save.py
+++ db/image_save.py
@@ -4,14 +4,15 @@ import os
 
 with open('db/url.csv') as images:
     reader = csv.reader(images)
-
+    num = 1
     for row in reader:
         try:
-            url = row[1]
-            filename = 'images/{}'.format(row[0])
+            url = row[2]
+            filename = 'Yogic_sleep_pose/{}'.format(num)
             if not os.path.exists('images'):
                 os.makedirs('images')
             print('Saving', url, 'to', filename)
             urllib.request.urlretrieve(url, filename)
+            num += 1
         except Exception as e:
             print('Failed to download image:', e)
